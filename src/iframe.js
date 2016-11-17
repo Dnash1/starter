3a3bd20fdeefc5504491dd7fbdb4d67da15ea236
David Nash <davidnash@Davids-MacBook-Pro.local>
2016-11-17 09:32:45 -0500

    add content, add js

---
+++ src/iframe.js
@@ -0,0 +1,5 @@
+console.log(" ▲ ")
+console.log("▲ ▲")
+document.getElementById("close").addEventListener("click", function( event ) {
+	parent.removeIframe();
+}, false);
---
+++ src/parent.js
@@ -0,0 +1,4 @@
+
+function removeIframe() {
+	document.querySelector(".stay-wanderful-iframe").remove();
+}
